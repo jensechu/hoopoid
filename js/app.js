eb0687198e29a8e5baa8cb821c45b40534173fbb
Jensen Hussey <jensen@jensechu.com>
2010-11-29 03:40:11 -0500

    Cleaned up the code and added sets

--- js/app.js
+++ js/app.js
@@ -1,50 +1,39 @@
 $(function(){
     
-    var paper = Raphael("cloud", 400, 400);
-    var back = paper.rect(0, 0, 400, 400);
-    back.attr({fill: "#000"});
-
+    var paper = Raphael(document.getElementById("hoop"), 400, 400);
+    
     var hoop_box = paper.rect(0, 0, 400, 400);
-    hoop_box.attr({fill: "#000", stroke: "grey", "stroke-width": 10});
+    hoop_box.attr({fill: "black"});
 
+    //Create hoop and base tape color
     var hoop = paper.circle(195, 200, 150, 150);
-    hoop.attr({stroke: "red", "stroke-width": 10});
+    hoop.attr({stroke: "#ff6699", "stroke-width": 10});
     
-    var tape_path = paper.path("M 40 200 C 75 100 175 100 200 200 S 305 300 350 200");
-    var tape = tape_path.attr({stroke: "white", "stroke-width": 3});
-    
-    function taping(){
+    //Path for all tapes
+    var tape_path = paper.path("M 40 200 C 75 100 175 100 200 200 S 305 300 350 200").hide();
+
+    //Tape generator
+    function tape_generator(set){
 	i = 0
-	while (i < 360) {	    
-	    tape.clone().rotate(i);
+	while (i < 360) {
+	    set.push(
+		tape_path.clone().rotate(i)
+	    );
 	    i += 10;
 	}
+	return set;
     }
 
-    taping();
-
-    var tape_path2 = paper.path("M 40 200 C 75 100 175 100 200 200 S 305 300 350 200");
-    var grip_tape = tape_path2.attr({stroke: "green", "stroke-width": 1});
+    //Secondary tape generator
+    var secondary_set = tape_generator(paper.set()).attr({stroke: "white", "stroke-width": 4});
     
-    function taping2(){
-	i = 0
-	while (i < 360) {	    
-	    grip_tape.clone().rotate(i);
-	    i += 10;
-	}
-    }
+    //Grip tape generator
+    var grip_set = tape_generator(paper.set()).attr({stroke: "green", "stroke-width": 2});
+     
+    //Smooth out the tape edges    
+    var inside_hoop = paper.circle(195, 200, 146, 146);
+    inside_hoop.attr({fill: "#000"});
+    var outside_hoop = paper.circle(195, 200, 156, 156);
+    outside_hoop.attr({stroke: "#000", "stroke-width": 5});
 
-    taping2();
-
-    var hoop2 = paper.circle(195, 200, 146, 146);
-    hoop2.attr({fill: "black"});
-    var hoop3 = paper.circle(195, 200, 156, 156);
-    hoop3.attr({stroke: "black", "stroke-width": 5});
- 
-/*    var tape = paper.path("M200 200L40 400, M200 200L10 400");
-    tape.attr({stroke: "lightblue", "stroke-width": 10});
-    
-*/
-
-    
-})
+ })
