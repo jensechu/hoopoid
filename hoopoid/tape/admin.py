eb1edb0aae6663d9db6f45ce4e68d883319f4d4d
Jensen Hussey <jensechu@gmail.com>
2011-06-18 21:38:08 -0400

    Making the admin use the unicode conversion function to display properly on the interface.

--- hoopoid/tape/admin.py
+++ hoopoid/tape/admin.py
@@ -1,5 +1,8 @@
 from hoopoid.tape.models import Type, Tape
 from django.contrib import admin
 
+class TapeAdmin(admin.ModelAdmin):
+    list_display = ('tape_type', 'color', 'quantity')
+
 admin.site.register(Type)
-admin.site.register(Tape)
+admin.site.register(Tape, TapeAdmin)
--- hoopoid/tape/models.py
+++ hoopoid/tape/models.py
@@ -1,16 +1,13 @@
 from django.db import models
 
-
 class Type(models.Model):
-    TAPE_TYPES = (
-        ('grip', 'Grip'),
-        ('base', 'Base'),
-        ('accent', 'Accent')
-        )
-    tape_type = models.CharField(max_length=10, choices=TAPE_TYPES)
+    name = models.CharField(max_length=100)
+    
+    def __unicode__(self):
+        return self.name
+    __str__ = __unicode__
 
 class Tape(models.Model):
     tape_type = models.ForeignKey(Type)
     color = models.CharField(max_length=100)
     quantity = models.IntegerField()
-    
